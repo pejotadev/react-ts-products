137ad5c8b0565f8a238ad0c66730cbec874bce0a
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-07 13:56:03 -0300

    feat: using purchaseModal

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { useDataContext } from '../../Context/DataContext';
 import CartSVG from '../../assets/CartSVG';
+import { PurchaseModal } from '../PurchaseModal/PurchaseModal';
 import { Search } from '../Search/Search';
 import './Header.css';
 
 export const Header = () => {
   const {cart, totalPrice} = useDataContext();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   return (
     <header className="header">
@@ -15,12 +18,17 @@ export const Header = () => {
 
       <Search />
 
-      <div className="cart-menu">
+      <div className="cart-menu" onClick={() => setIsModalOpen(true)}> 
         <CartSVG 
           title={'Ver Carrinho de compras'} 
           color='currentColor' />
         {cart.length} | {totalPrice.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}
       </div>
+
+      <PurchaseModal 
+        isModalOpen={isModalOpen} 
+        toggleModal={() => setIsModalOpen(!isModalOpen)} 
+        />
     </header>
   )
 }
