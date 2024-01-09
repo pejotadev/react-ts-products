47267476d01f94b7c40595e3275ef7a41285d5ae
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-09 14:31:36 -0300

    refactor: Interface para as props do PurchaseModal

--- src/Components/PurchaseModal/PurchaseModal.tsx
+++ src/Components/PurchaseModal/PurchaseModal.tsx
@@ -1,16 +1,19 @@
+import React from 'react';
 import { PurchaseList } from '../PurchaseList/PurchaseList';
-import Modal from '../Utils/Modal'
+import Modal from '../Utils/Modal';
 
-export const PurchaseModal = ({isModalOpen, toggleModal} : {
+interface PurchaseModalProps {
   isModalOpen: boolean;
   toggleModal: () => void;
-}) => {
+}
+
+export const PurchaseModal: React.FC<PurchaseModalProps> = ({ isModalOpen, toggleModal }) => {
   return (
     <Modal isOpen={isModalOpen} onClose={toggleModal}>
-        <h2>Suas Compras</h2>
-        <div>
-          <PurchaseList />
-        </div>
+      <h2>Suas Compras</h2>
+      <section>
+        <PurchaseList />
+      </section>
     </Modal>
-  )
+  );
 }
